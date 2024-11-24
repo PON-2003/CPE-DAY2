5c600b051fc493e24b826c73cfd467589495dd2a
tawan.sermsuk <tawan.sermsuk@gmail.com>
2024-11-24 10:57:48 +0700

    add route users products category

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/', async (req, res) => {
     res.json(result);
 });
 
+app.get('/users', async (req, res) => {
+    const result = await db.query("select * FROM users");
+    res.json(result);
+});
+
+app.get('/category', async (req, res) => {
+    const result = await db.query("select * FROM categories");
+    res.json(result);
+});
+
+app.get('/products', async (req, res) => {
+    const result = await db.query(`
+        SELECT * 
+        FROM products
+        INNER JOIN product_details ON products.product_id = product_details.product_id
+        INNER JOIN categories ON products.category_id = categories.category_id
+    `);
+    res.json(result);
+});
+
 app.listen(3000, () => {
     console.log("API Running");
 });
